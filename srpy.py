fab8810fba3d892e9ba31ed57d5a606a98e343b8
Joseph Brown <84574944+JosephSBrown@users.noreply.github.com>
2023-01-25 20:12:22 +0000

    Add files via upload

---
+++ srpy.py
@@ -0,0 +1,78 @@
+import speech_recognition as sr
+
+def speechrecognition_en():
+    
+    r = sr.Recognizer()
+    
+    while True:
+        try:
+            with sr.Microphone() as mic:
+
+                r.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = r.listen(mic)
+
+                text = r.recognize_google(audio)
+
+                print(f'Heard: {text}')
+                return
+
+        except sr.UnknownValueError():
+            r = sr.Recognizer()
+            continue
+
+def speechrecognition_jp():
+    
+    r = sr.Recognizer()
+
+    while True:
+        try:
+            with sr.Microphone() as mic:
+
+                r.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = r.listen(mic)
+
+                text = r.recognize_google(audio, language='ja-JP')
+
+                print(f'Heard: {text}')
+                return
+
+        except sr.UnknownValueError():
+            r = sr.Recognizer()
+            continue
+
+def speechrecognition_fr():
+    
+    r = sr.Recognizer()
+
+    while True:
+        try:
+            with sr.Microphone() as mic:
+
+                r.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = r.listen(mic)
+
+                text = r.recognize_google(audio, language='fr-FR')
+
+                print(f'Heard: {text}')
+                return
+
+        except sr.UnknownValueError():
+            r = sr.Recognizer()
+            continue
+
+def App():
+    print('If You Wish to Speak in English Press 1')
+    print('If You Wish to Speak in Japanese Press 2')
+    print('If You Wish to Speak in French Press 3')
+    o = input('Option: ')
+    if o == '1':
+        speechrecognition_en()
+    elif o == '2':
+        speechrecognition_jp()
+    elif o == '3':
+        speechrecognition_fr()
+    else:
+        App()
+
+
+App()
